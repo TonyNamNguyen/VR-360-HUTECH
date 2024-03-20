aa805c374f0933953721ec4469b5e9dc0575e5ec
Nguyen Trinh Nam Trung <nguyentrinhnamtrung22@gmail.com>
2024-03-20 14:40:39 +0700

    upload

---
+++ Data/js/main.js
@@ -0,0 +1,4 @@
+function toggleMap() {
+    var mapContainer = document.querySelector('.map-container');
+    mapContainer.style.display = (mapContainer.style.display === 'none') ? 'block' : 'none';
+  }
